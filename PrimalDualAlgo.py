fb5c48bf4ca68c7b225dc078a30b52215af3ff57
Theresa Wettig <theresa.wettig@tum.de>
2021-06-01 02:09:00 +0200

    Refactored file names

--- PrimalDualAlgo.py
+++ PrimalDualAlgo.py
@@ -4,7 +4,7 @@ import gurobipy as gp
 import NEW_UnrestrDual as nfd
 import NEW_RestrictedProblems as nr
 import Z_Depr_WithoutGRB as wgrb
-import New_Algo_Without_Gurobi as wgrbn
+import NEW_AlgoWithoutGurobi as wgrbn
 import numpy as np
 import time
 
